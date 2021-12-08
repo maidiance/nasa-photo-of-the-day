a0fee72cdec15422710b4ea8ba9ab00fc2c25543
Winifred Lu <winnie.lu@comcast.net>
2021-12-08 14:51:59 -0500

    initial state and axios call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(res => {
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }, []);
+
   return (
     <div className="App">
       <p>
